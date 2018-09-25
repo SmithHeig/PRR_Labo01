b6f777bf54c19c5387a9156eb8a7d626a0a9a3a8
James <james.smith@heig-vd.ch>
2018-09-25 17:01:02 +0200

    Création project

---
+++ src/Slave_Labo01/src/prr/labo/GapSlave.java
@@ -0,0 +1,9 @@
+package prr.labo;
+
+public class GapSlave implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/Slave_Labo01/src/prr/labo/Slave.java
@@ -0,0 +1,24 @@
+package prr.labo;
+
+import java.util.Date;
+
+public class Slave {
+    private long delay;
+    private long gap;
+    boolean delaySlaveIsRunning;
+
+    /** Construscteur**/
+    public Slave(){
+        delay = 0;
+        gap = 0;
+        delaySlaveIsRunning = false;
+    }
+
+    private void gapSlave(){
+
+    }
+
+    private void delaySlave(){
+
+    }
+}
